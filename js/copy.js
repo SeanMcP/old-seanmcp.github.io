4be8faa97af89a8e620434c2c6de241abd61c20a
Sean McPherson <seanmcp25@gmail.com>
2018-05-12 20:58:07 -0400

    Adjust styles
    Reorganized file naming conventions

--- js/copy.js
+++ js/copy.js
@@ -3,9 +3,9 @@ const copy = {
         heading: 'Home',
         tagline: 'Home tagline',
         content: [
-            'Some text.',
-            'Some more.',
-            'Why not a third?'
+            'Integer nec sapien venenatis, volutpat tellus molestie, tempor urna. Proin vehicula rutrum diam, id eleifend ipsum tincidunt eu. Integer a mattis orci. In non tortor faucibus, faucibus nulla sit amet, interdum libero.',
+            'Suspendisse vel mi tristique, malesuada metus non, sagittis ligula. Mauris mollis accumsan ullamcorper. Phasellus in justo a nisi convallis finibus. Praesent pretium fringilla vestibulum. Sed sit amet gravida odio. Vestibulum vitae consectetur metus, vitae placerat erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+            'In pulvinar, diam vitae convallis mollis, massa velit venenatis felis, in hendrerit ex nibh ac ipsum. Donec libero odio, sollicitudin molestie orci eu, viverra posuere mi. Aliquam libero libero, tempor nec mi sit amet, porttitor sollicitudin urna. Phasellus id velit nunc.'
         ],
         image: null
     },
@@ -13,9 +13,9 @@ const copy = {
         heading: 'About',
         tagline: 'About tagline',
         content: [
-            'Some text.',
-            'Some more.',
-            'Why not a third?'
+            'Integer nec sapien venenatis, volutpat tellus molestie, tempor urna. Proin vehicula rutrum diam, id eleifend ipsum tincidunt eu. Integer a mattis orci. In non tortor faucibus, faucibus nulla sit amet, interdum libero.',
+            'Suspendisse vel mi tristique, malesuada metus non, sagittis ligula. Mauris mollis accumsan ullamcorper. Phasellus in justo a nisi convallis finibus. Praesent pretium fringilla vestibulum. Sed sit amet gravida odio. Vestibulum vitae consectetur metus, vitae placerat erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+            'In pulvinar, diam vitae convallis mollis, massa velit venenatis felis, in hendrerit ex nibh ac ipsum. Donec libero odio, sollicitudin molestie orci eu, viverra posuere mi. Aliquam libero libero, tempor nec mi sit amet, porttitor sollicitudin urna. Phasellus id velit nunc.'
         ],
         image: null
     },
@@ -23,9 +23,9 @@ const copy = {
         heading: 'Work',
         tagline: 'Work tagline',
         content: [
-            'Some text.',
-            'Some more.',
-            'Why not a third?'
+            'Integer nec sapien venenatis, volutpat tellus molestie, tempor urna. Proin vehicula rutrum diam, id eleifend ipsum tincidunt eu. Integer a mattis orci. In non tortor faucibus, faucibus nulla sit amet, interdum libero.',
+            'Suspendisse vel mi tristique, malesuada metus non, sagittis ligula. Mauris mollis accumsan ullamcorper. Phasellus in justo a nisi convallis finibus. Praesent pretium fringilla vestibulum. Sed sit amet gravida odio. Vestibulum vitae consectetur metus, vitae placerat erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+            'In pulvinar, diam vitae convallis mollis, massa velit venenatis felis, in hendrerit ex nibh ac ipsum. Donec libero odio, sollicitudin molestie orci eu, viverra posuere mi. Aliquam libero libero, tempor nec mi sit amet, porttitor sollicitudin urna. Phasellus id velit nunc.'
         ],
         image: null
     },
@@ -33,9 +33,9 @@ const copy = {
         heading: 'Contact',
         tagline: 'Contact tagline',
         content: [
-            'Some text.',
-            'Some more.',
-            'Why not a third?'
+            'Integer nec sapien venenatis, volutpat tellus molestie, tempor urna. Proin vehicula rutrum diam, id eleifend ipsum tincidunt eu. Integer a mattis orci. In non tortor faucibus, faucibus nulla sit amet, interdum libero.',
+            'Suspendisse vel mi tristique, malesuada metus non, sagittis ligula. Mauris mollis accumsan ullamcorper. Phasellus in justo a nisi convallis finibus. Praesent pretium fringilla vestibulum. Sed sit amet gravida odio. Vestibulum vitae consectetur metus, vitae placerat erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+            'In pulvinar, diam vitae convallis mollis, massa velit venenatis felis, in hendrerit ex nibh ac ipsum. Donec libero odio, sollicitudin molestie orci eu, viverra posuere mi. Aliquam libero libero, tempor nec mi sit amet, porttitor sollicitudin urna. Phasellus id velit nunc.'
         ],
         image: null
     },
