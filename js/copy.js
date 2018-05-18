c2781082f6b3f33c616d39ee08626f00766bdd7c
Sean McPherson <seanmcp25@gmail.com>
2018-05-17 20:40:34 -0400

    Tweak styles

--- js/copy.js
+++ js/copy.js
@@ -1,11 +1,9 @@
 const copy = {
     home: {
-        heading: 'Home',
-        tagline: 'Home tagline',
+        heading: 'Sean McPherson',
+        tagline: 'Software developer and problem solver',
         content: [
             'Integer nec sapien venenatis, volutpat tellus molestie, tempor urna. Proin vehicula rutrum diam, id eleifend ipsum tincidunt eu. Integer a mattis orci. In non tortor faucibus, faucibus nulla sit amet, interdum libero.',
-            'Suspendisse vel mi tristique, malesuada metus non, sagittis ligula. Mauris mollis accumsan ullamcorper. Phasellus in justo a nisi convallis finibus. Praesent pretium fringilla vestibulum. Sed sit amet gravida odio. Vestibulum vitae consectetur metus, vitae placerat erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-            'In pulvinar, diam vitae convallis mollis, massa velit venenatis felis, in hendrerit ex nibh ac ipsum. Donec libero odio, sollicitudin molestie orci eu, viverra posuere mi. Aliquam libero libero, tempor nec mi sit amet, porttitor sollicitudin urna. Phasellus id velit nunc.'
         ],
         image: null
     },
@@ -33,6 +31,9 @@ const copy = {
         heading: 'Contact',
         tagline: 'Contact tagline',
         content: [
+            'Integer nec sapien venenatis, volutpat tellus molestie, tempor urna. Proin vehicula rutrum diam, id eleifend ipsum tincidunt eu. Integer a mattis orci. In non tortor faucibus, faucibus nulla sit amet, interdum libero.',
+            'Suspendisse vel mi tristique, malesuada metus non, sagittis ligula. Mauris mollis accumsan ullamcorper. Phasellus in justo a nisi convallis finibus. Praesent pretium fringilla vestibulum. Sed sit amet gravida odio. Vestibulum vitae consectetur metus, vitae placerat erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+            'In pulvinar, diam vitae convallis mollis, massa velit venenatis felis, in hendrerit ex nibh ac ipsum. Donec libero odio, sollicitudin molestie orci eu, viverra posuere mi. Aliquam libero libero, tempor nec mi sit amet, porttitor sollicitudin urna. Phasellus id velit nunc.',
             'Integer nec sapien venenatis, volutpat tellus molestie, tempor urna. Proin vehicula rutrum diam, id eleifend ipsum tincidunt eu. Integer a mattis orci. In non tortor faucibus, faucibus nulla sit amet, interdum libero.',
             'Suspendisse vel mi tristique, malesuada metus non, sagittis ligula. Mauris mollis accumsan ullamcorper. Phasellus in justo a nisi convallis finibus. Praesent pretium fringilla vestibulum. Sed sit amet gravida odio. Vestibulum vitae consectetur metus, vitae placerat erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
             'In pulvinar, diam vitae convallis mollis, massa velit venenatis felis, in hendrerit ex nibh ac ipsum. Donec libero odio, sollicitudin molestie orci eu, viverra posuere mi. Aliquam libero libero, tempor nec mi sit amet, porttitor sollicitudin urna. Phasellus id velit nunc.'
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // HTML elements
 
+const body = document.querySelector('body');
 const root = document.getElementById('root');
 const main = document.querySelector('main');
 const nav = document.querySelector('nav');
@@ -15,16 +16,16 @@ writeNavOptions(nav);
 
 if (hash.length) {
     const slice = hash.slice(1);
-    main.classList.add(slice);
+    body.classList.add(slice);
     document.querySelector(`a[href='${hash}']`).classList.add('active', slice);
 } else {
-    main.classList.add('home');
+    body.classList.add('home');
     document.querySelector(`a[href='#home']`).classList.add('active', 'home');
 }
 
 
 window.onhashchange = () => {
     writeSection(main);
-    main.removeAttribute('class');
-    main.classList.add(window.location.hash.slice(1));
+    body.removeAttribute('class');
+    body.classList.add(window.location.hash.slice(1));
 };
